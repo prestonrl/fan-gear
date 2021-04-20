b92b5b40ff68ecbd4956c52d435dc48c76f856a9
tubasthebest <tubasthebest@gmail.com>
2021-04-19 21:43:08 -0600

    updated models and started seed

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,25 +1,36 @@
-const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 
 const productSchema = new Schema({
-    productId: {
+    name: {
         type: String,
         required: true,
-    },
-    price: {
-        type: Int,
-        required: true,
+        trim: true
     },
     description: {
-        type: String,
-        required: true,
-    },
-    title: {
-        type: String,
-        required: true,
+        type: String
     },
     image: {
-        type: String,
-    }    
+        type: String
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0.99
+    },
+    quantity: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
 });
 
-module.exports = productSchema;
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
--- server/models/User.js
+++ server/models/User.js
@@ -1,38 +1,35 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
+const Order = require('./Order');
 
-// import schema from Book.js
-const productSchema = require('./Product');
-
-const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [/.+@.+\..+/, 'Must use a valid email address'],
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        // set savedProducts to be an array of data that adheres to the productSchema
-        savedProducts: [productSchema],
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
     },
-    // set this to use virtual below
-    {
-        toJSON: {
-            virtuals: true,
-        },
-    }
-);
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    orders: [Order.schema],
+});
 
-// hash user password
+// set up pre-save middleware to create password
 userSchema.pre('save', async function (next) {
     if (this.isNew || this.isModified('password')) {
         const saltRounds = 10;
@@ -42,16 +39,11 @@ userSchema.pre('save', async function (next) {
     next();
 });
 
-// custom method to compare and validate password for logging in
+// compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-userSchema.virtual('bookCount').get(function () {
-    return this.savedProducts.length;
-});
-
-const User = model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
