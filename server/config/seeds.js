7f17a3a335aaea8544080cc560a52eb45de26eb1
tubasthebest <tubasthebest@gmail.com>
2021-04-22 19:59:46 -0600

    server and seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -5,10 +5,8 @@ db.once('open', async () => {
     await Category.deleteMany();
 
     const categories = await Category.insertMany([
-        { name: 'Hats' },
+        { name: 'Fedoras' },
         { name: 'Jackets' },
-        { name: 'Whips' },
-        { name: 'Clothes' },
         { name: 'Accessories' }
     ]);
 
@@ -20,108 +18,105 @@ db.once('open', async () => {
         {
             name: 'Explorador \'36',
             description:
-                'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-            image: 'cookie-tin.jpg',
+                'Raiders',
+            image: 'raiders-hat.jpg',
             category: categories[0]._id,
-            price: 2.99,
+            price: 220.00,
             quantity: 500
         },
         {
-            name: 'Canned Coffee',
+            name: 'Explorador \'35',
             description:
-                'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-            image: 'canned-coffee.jpg',
+                'Temple',
+            image: 'TOD-hat.jpg',
             category: categories[0]._id,
-            price: 1.99,
+            price: 220.00,
             quantity: 500
         },
         {
-            name: 'Toilet Paper',
-            category: categories[1]._id,
+            name: 'Explorador \'38',
+            category: categories[0]._id,
             description:
-                'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-            image: 'toilet-paper.jpg',
-            price: 7.99,
+                'Last Crusade',
+            image: 'crusade-hat.jpg',
+            price: 220.00,
             quantity: 20
         },
         {
-            name: 'Handmade Soap',
-            category: categories[1]._id,
+            name: 'Explorador \'57',
+            category: categories[0]._id,
             description:
-                'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-            image: 'soap.jpg',
-            price: 3.99,
+                'Crystal Skull',
+            image: 'CS-hat.jpg',
+            price: 220.00,
             quantity: 50
         },
         {
-            name: 'Set of Wooden Spoons',
+            name: 'Viajero \'36',
+            category: categories[0]._id,
+            description:
+                'Raiders travel hat',
+            image: 'raiders-grey.jpg',
+            price: 220.00,
+            quantity: 100
+        },
+        {
+            name: 'Viajero \'57',
+            category: categories[0]._id,
+            description:
+                'Crystall skull travel hat',
+            image: 'CS-grey.jpg',
+            price: 220.00,
+            quantity: 30
+        },
+        {
+            name: 'Cazadora \'36 -Raiders-',
             category: categories[1]._id,
             description:
-                'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-            image: 'wooden-spoons.jpg',
-            price: 14.99,
-            quantity: 100
-        },
-        {
-            name: 'Camera',
-            category: categories[2]._id,
-            description:
-                'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-            image: 'camera.jpg',
-            price: 399.99,
+                'The TRUCK jacket. Correct lambskin leather -color and texture-, correct lining, correct zipper, snaps and buckles, and distressing if wanted.',
+            image: 'raiders-jacket.jpg',
+            price: 555.00,
             quantity: 30
         },
         {
-            name: 'Tablet',
+            name: 'CAZADORA \'38 -Last Crusade-',
+            category: categories[1]._id,
+            description:
+                'Correct lambskin leather -color and texture- or "Chocolate Premium Horsehide", correct lining, correct zipper, snaps and buckles, and distressing if wanted.',
+            image: 'crusade-jacket.jpg',
+            price: 525.00,
+            quantity: 100
+        },
+        {
+            name: 'Leather Accessories -Model \'36',
             category: categories[2]._id,
-            description:
-                'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-            image: 'tablet.jpg',
-            price: 199.99,
-            quantity: 30
-        },
-        {
-            name: 'Tales at Bedtime',
-            category: categories[3]._id,
-            description:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-            image: 'bedtime-book.jpg',
-            price: 9.99,
-            quantity: 100
-        },
-        {
-            name: 'Spinning Top',
-            category: categories[4]._id,
-            description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-            image: 'spinning-top.jpg',
-            price: 1.99,
+            description: 'Combo: Holster, belt, whipholder',
+            image: 'raiders-accessories.jpg',
+            price: 120.00,
             quantity: 1000
         },
         {
-            name: 'Set of Plastic Horses',
-            category: categories[4]._id,
-            description:
-                'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-            image: 'plastic-horses.jpg',
-            price: 2.99,
+            name: 'Leather Accessories -Model \'35',
+            category: categories[2]._id,
+            description: 'Combo: Holster, belt, whipholder',
+            image: 'TOD-accessories.jpg',
+            price: 120.00,
             quantity: 1000
         },
         {
-            name: 'Teddy Bear',
-            category: categories[4]._id,
-            description:
-                'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-            image: 'teddy-bear.jpg',
-            price: 7.99,
+            name: 'Leather Accessories -Model \'38',
+            category: categories[2]._id,
+            description: 'Combo: Holster, belt, whipholder',
+            image: 'crusade-accessories.jpg',
+            price: 120.00,
             quantity: 100
         },
         {
-            name: 'Alphabet Blocks',
-            category: categories[4]._id,
-            description:
-                'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-            image: 'alphabet-blocks.jpg',
-            price: 9.99,
+            name: 'Leather Accessories -Model \'57',
+            category: categories[2]._id,
+            description: 'Combo: Holster, belt, whipholder',
+            image: 'CS-accessories.jpg',
+            price: 120.00,
             quantity: 600
         }
     ]);
