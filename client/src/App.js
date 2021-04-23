f7fa352575ea6cf46b355f57e5da54956f7ffc05
tubasthebest <tubasthebest@gmail.com>
2021-04-22 21:28:19 -0600

    frontend set up

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import './App.css';
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+import { StoreProvider } from "./utils/GlobalState";
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
+
+  );
+}
+
+export default App;
