e19e2be994784788521f55161003048a1ae40152
Dave <bbec0689@gmail.com>
2021-04-26 20:18:24 -0600

    created product page styling

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -4,6 +4,7 @@ import { pluralize } from "../../utils/helpers"
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import Button from 'react-bootstrap/Button';
 
 function ProductItem(item) {
     const [state, dispatch] = useStoreContext();
@@ -40,19 +41,19 @@ function ProductItem(item) {
     }
 
     return (
-        <div className="card px-1 py-1">
+        <div className="card px-1 py-1 text-color Jones product-styling">
             <Link to={`/products/${_id}`}>
                 <img
                     alt={name}
                     src={`/images/${image}`}
                 />
-                <p>{name}</p>
+                <p className="underline">{name}</p>
             </Link>
             <div>
                 <div>{quantity} {pluralize("item", quantity)} in stock</div>
                 <span>${price}</span>
             </div>
-            <button onClick={addToCart}>Add to cart</button>
+            <Button className="Jones" onClick={addToCart}>Add to cart</Button>
         </div>
     );
 }
