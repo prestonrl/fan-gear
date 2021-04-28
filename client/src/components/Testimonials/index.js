080857f59d52c3a41203d5779ec3b6a96b71d7c7
hunterstrunk <hunter@strunkfam.com>
2021-04-27 19:57:44 -0600

    changed styling for testimonials

--- client/src/components/Testimonials/index.js
+++ client/src/components/Testimonials/index.js
@@ -1,16 +1,4 @@
 import React from 'react';
-import expl59 from '../../assets/expl59.jfif';
-import caza36 from '../../assets/caza36.jpg';
-import expl1 from '../../assets/expl1.jfif';
-import expl67 from '../../assets/expl67.jfif';
-import expl16 from '../../assets/expl16.jfif';
-import expl70 from '../../assets/expl70.jfif';
-import expl71 from '../../assets/expl71.jfif';
-import expl29 from '../../assets/expl29.jfif';
-import expl49 from '../../assets/expl49.jfif';
-import expl10 from '../../assets/expl10.jfif';
-import expl11 from '../../assets/expl11.jfif';
-import expl12 from '../../assets/expl12.jfif';
 
 function Testimonials() {
     return(
@@ -19,9 +7,9 @@ function Testimonials() {
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Cazadora '36</h3>
+                    <h3 className='text-color Jones testimonial-title'>Cazadora '36</h3>
 
-                    <p>
+                    <p className='testimonial-text'>
                     "Since 2006 I've owned quite a number of Indiana Jones Jackets from other manufacturers. 
                     All the while I've been searching for the perfect Raiders of the Lost Ark jacket with all 
                     it's perculiar features, each of which is necessary in order to get the 'look'. Id begun to 
@@ -34,33 +22,30 @@ function Testimonials() {
                     but it is far and away the most accurate in appearance. Great work guys from a satisfied customer."
                     </p>
 
-                    <h6 className='text-color Jones'>-Derek (UK)</h6>
+                    <h5 className='text-color Jones testimonial-subtitle'>-Derek (UK)</h5>
                 </div>
 
-                <img className="float-image testimonial-images" src={caza36} alt=""></img>
             </div>
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #1 (Raiders)</h3>
+                    <h3 className='text-color Jones testimonial-title'>Explorador #1 (Raiders)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "What can I say. FANTASTIC !!!!!! Thank you very, very, very much Harry and Indiego. 
                     Bashing great, soft touch, Excellent work. Seems to have been taken from Indy at Raiders 
                     and placed in my hands."
                     </p>
     
-                    <h6 className='text-color Jones'>-Harryford Jones (Tarragona, ESPAÑA)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-Harryford Jones (Tarragona, ESPAÑA)</h6>
                 </div>
-
-                <img className="float-image testimonial-images" src={expl1} alt=""></img>
             </div>
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #67 (Crusade)</h3>
+                    <h3 className='text-color Jones testimonial-title'>Explorador #67 (Crusade)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "I'm speechless. So i will proceed to a breakdown of pros and cons of this fedora: PROS:
                     -first sensation right out of the box: touch. fine felt and dense. lots of quality. it feels to resist everything you throw at it. feels like mistreat instead of take care of it.
                     -second impression: bashing and appearance. seems straight out of the movie. from ribbon and bow to the crown and brim are all accurately reproduced. even have to pull down the brim when you put it on, Ford's style.
@@ -68,18 +53,17 @@ function Testimonials() {
                     CONS:
                     -haven't ordered it before. now i'd have an Explorador and a Viajero."
                     </p>
-    
-                    <h6 className='text-color Jones'>-Montana Smith (Madrid, ESPAÑA)</h6>
+
+                    <h6 className='text-color Jones testimonial-subtitle'>Montana Smith (Madrid, ESPAÑA)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl67} alt=""></img>
             </div>
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #16 (Skull)</h3>
+                    <h3 className='text-color Jones testimonial-title'>Explorador #16 (Skull)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "...the hat perfectly pass thru a "completely wet"... raindrops fell from it, dripping all over 
                     (like the rest of my clothes ....) (Iguassu Falls) .. but after, was dry and perfect, 
                     spotless and without distortion ... as it did on the second test ... traveling in a suitcase 
@@ -87,61 +71,57 @@ function Testimonials() {
                     without touching it .... "
                     </p>
     
-                    <h6 className='text-color Jones'>-Jandemor (Madrid, ESPAÑA)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-Jandemor (Madrid, ESPAÑA)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl16} alt=""></img>
             </div>
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #49 (Raiders)</h3>
+                    <h3 className='text-color Jones testimonial-title'>Explorador #49 (Raiders)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "Thanks for everything, Harry and Indiego. You have sign and everything inside the felt! 
                     you are so cool, great! You did it!"
                     </p>
     
-                    <h6 className='text-color Jones'>-Chaval-ot (Madrid, ESPAÑA)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-Chaval-ot (Madrid, ESPAÑA)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl49} alt=""></img>
             </div>
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #70 (Raiders)</h3>
+                    <h3 className='text-color Jones testimonial-title'>Explorador #70 (Raiders)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "Harry & Indiego, thanks for a job very well done"
                     </p>
     
-                    <h6 className='text-color Jones'>-Indiana_jones_cl (Santiago, CHILE)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-Indiana_jones_cl (Santiago, CHILE)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl70} alt=""></img>
             </div>
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #71 (Raiders)</h3>
+                    <h3 className='text-color Jones testimonial-title'>Explorador #71 (Raiders)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "I want to thank alot to Harry and Diego. The hat is excellent, exceeded my expectations, thank you very much indeed ....
                     How Explorador resist the snow! Is an all purpose lid!!"
                     </p>
     
-                    <h6 className='text-color Jones'>-Marshall Dillon (Esquel, ARGENTINA)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-Marshall Dillon (Esquel, ARGENTINA)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl71} alt=""></img>
             </div>
 
-            <div className="form-a info-sections">
+            <div className="form-a info-sections ">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #10 (Raiders)</h3>
+                    <h3 className='text-color Jones testimonial-title'>Explorador #10 (Raiders)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "...it's great really have no words to describe it, is more, is what always had tried to find 
                     about shape, I love the bashing. Regarding the material is thin and smooth, and the best is not 
                     heavy, you can wear it with confidence .... I am a collector of Indiana's hats, with the 
@@ -149,64 +129,60 @@ function Testimonials() {
                     because it fills my expectations because, it's everything I've been waiting for."
                     </p>
     
-                    <h6 className='text-color Jones'>-Alberto Cisneros (Zürich, SUIZA)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-Alberto Cisneros (Zürich, SUIZA)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl10} alt=""></img>
             </div>
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #12 (Raiders)</h3>
+                    <h3 className='text-color Jones testimonial-title'>Explorador #12 (Raiders)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "I've got it! Damn, it's a realy cool hat. When I try it, I have to pull down the brim, 
                     making this gesture to my brother at the office was very exciting. It has good touch feeling, 
                     i'm still even afraid to touch it to much. My brother put it on and he has short hair and no 
                     beard and was totally look-alike..."
                     </p>
     
-                    <h6 className='text-color Jones'>-Isrbeth (Tudela -Navarra-, ESPAÑA)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-Isrbeth (Tudela -Navarra-, ESPAÑA)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl12} alt=""></img>
             </div>
 
-            <div className="form-a info-sections">
+            <div className="form-a info-sections ">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #11 (Raiders)</h3>
+                    <h3 className='text-color Jones testimonial-title'>Explorador #11 (Raiders)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "I can say now after a week than I have it, that this hat more than fulfilled everything I could expected ... is soft and very floppy."
                     </p>
     
-                    <h6 className='text-color Jones'>-Arsenero (Montevideo, URUGUAY)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-Arsenero (Montevideo, URUGUAY)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl11} alt=""></img>
             </div>
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #29 (Raiders)</h3>
+                    <h3 className='text-color Jones  testimonial-title'>Explorador #29 (Raiders)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "I want to add that it's an absolutely all purpose hat, I have used under several drizzles 
                     in Paris and fedora Explorador remains intact, I leave it to dry on the hotel room and recovered 
                     the bashing. I've had it on the bottom of the suitcase, and is as you can see in photos."
                     </p>
     
-                    <h6 className='text-color Jones'>-darth_ming (Santiago, CHILE)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-darth_ming (Santiago, CHILE)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl29} alt=""></img>
             </div>
 
             <div className="form-a info-sections">
                 <div>
-                    <h3 className='text-color Jones'>Explorador #59 (Raiders)</h3>
+                    <h3 className='text-color Jones  testimonial-title'>Explorador #59 (Raiders)</h3>
     
-                    <p>
+                    <p className='testimonial-text'>
                     "I trusted my first fedora to Harry and Indiego, an '36 Explorador bashed according to "Hovito's 
                     temple" scene. About a month ago it arrives and. ... IT'S GREAT, PERFECT, AND FAITHFUL TO THE FILM. 
                     These guys did a great job on this hat. Communication and resolution of my doubts has been excellent. 
@@ -214,10 +190,9 @@ function Testimonials() {
                     fans to make an order with these guys because you won't regret."
                     </p>
     
-                    <h6 className='text-color Jones'>-Indianamb (Alicante, ESPAÑA)</h6>
+                    <h6 className='text-color Jones testimonial-subtitle'>-Indianamb (Alicante, ESPAÑA)</h6>
                 </div>
 
-                <img className="float-image testimonial-images" src={expl59} alt=""></img>
             </div>
 
         </section>
